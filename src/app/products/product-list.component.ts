8bc12f45154c0050b8306b6682268b91da80e8fc
Mohamed EK <melkashef@takima.fr>
2021-03-08 18:23:48 +0100

    Handling errors

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
-import {Observable, Subscription} from 'rxjs';
+import {EMPTY, Observable, Subscription} from 'rxjs';
 
 import {Product} from './product';
 import {ProductService} from './product.service';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -20,7 +21,13 @@ export class ProductListComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
+    this.products$ = this.productService.getProducts()
+      .pipe(
+        catchError( err => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      );
   }
 
   onAdd(): void {
