64c519710ba5d3da31aaad619686d011b7a49a45
Mohamed EK <melkashef@takima.fr>
2021-03-23 15:05:55 +0100

    Hard coded simple filter by category

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,7 @@ import {EMPTY, Observable, Subscription} from 'rxjs';
 
 import {Product} from './product';
 import {ProductService} from './product.service';
-import {catchError} from 'rxjs/operators';
+import {catchError, filter, map} from 'rxjs/operators';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -15,6 +15,7 @@ export class ProductListComponent  {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
+  selectedCategoryId = 1;
 
   products$ = this.productService.productsWithCategory$
     .pipe(
@@ -23,6 +24,13 @@ export class ProductListComponent  {
       return EMPTY;
     })
   );
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$
+    .pipe(
+      map( products =>
+        products.filter( product => this.selectedCategoryId ?  product.categoryId === this.selectedCategoryId : true )
+      )
+    );
 
   sub: Subscription;
 
