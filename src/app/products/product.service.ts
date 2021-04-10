2e5983366e3803b34f80a61b63a850c7ed711062
Mohamed EK <melkashef@takima.fr>
2021-04-10 08:43:09 +0200

    Related data : Just in time strategy

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import {BehaviorSubject, combineLatest, merge, Observable, Subject, throwError} from 'rxjs';
-import {catchError, filter, map, scan, shareReplay, tap} from 'rxjs/operators';
+import {BehaviorSubject, combineLatest, from, merge, Observable, Subject, throwError} from 'rxjs';
+import {catchError, filter, map, mergeMap, scan, shareReplay, switchMap, tap, toArray} from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -54,12 +54,25 @@ export class ProductService {
       shareReplay(1)
     );
 
-  selectedProductSuppliers$ = combineLatest([
-    this.selectedProduct$,
-    this.supplierService.suppliers$
-  ])
+  // selectedProductSuppliers$ = combineLatest([
+  //   this.selectedProduct$,
+  //   this.supplierService.suppliers$
+  // ])
+  //   .pipe(
+  //    map(([selectedProduct, suppliers]) => suppliers.filter(supplier => selectedProduct.supplierIds.includes(supplier.id)))
+  //   );
+
+  selectedProductSuppliers$ = this.selectedProduct$
     .pipe(
-     map(([selectedProduct, suppliers]) => suppliers.filter(supplier => selectedProduct.supplierIds.includes(supplier.id)))
+      filter(selectedProduct => Boolean(selectedProduct)),
+      switchMap(selectedProduct =>
+        from(selectedProduct.supplierIds)
+          .pipe(
+            mergeMap(supplierId => this.http.get<Supplier>(`${this.suppliersUrl}/${supplierId}`)),
+            toArray(),
+            tap(suppliers => console.log('product suppliers', JSON.stringify(suppliers)))
+          )
+      )
     );
 
   private productInsertedASubject = new Subject<Product>();
